ab4749beec72d80852487f761a0c16e258bba62e
seomaks <m.voevodin88@gmail.com>
2021-11-11 16:34:11 +0300

    componentDidMount - Users page

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,12 +5,10 @@ import userPhoto from '../../assets/images/user.png'
 import {UsersPropsType} from "./UsersContainer";
 
 class Users extends React.Component<UsersPropsType> {
-
-  constructor(props: UsersPropsType) {
-    super(props);
-      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        this.props.setUsers(response.data.items)
-      });
+  componentDidMount() {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      this.props.setUsers(response.data.items)
+    });
   }
 
   render() {
