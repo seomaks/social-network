c9ebc33ddf4687206baf36956a4fc71e0770f77e
seomaks <m.voevodin88@gmail.com>
2022-01-14 09:13:42 +0300

    Fix the problem in the ProfileContainer component

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -4,6 +4,76 @@ import {connect} from "react-redux";
 import {
   getStatus,
   getUserProfile,
+  updateStatus,
+} from "../../redux/profileReducer";
+import {AppStateType} from "../../redux/redux-store";
+import {RouteComponentProps, withRouter} from "react-router-dom";
+import {compose} from "redux";
+
+type PathParamsType = {
+  userId: string
+}
+
+type MapStateToPropsType = ReturnType<typeof mapStateToProps>
+
+
+type MapDispatchPropsType = {
+  getUserProfile: (userId: number) => void
+  getStatus: (userId: number |string | null) => void
+  updateStatus: (status: string) => void
+}
+type OwnPropsType = MapStateToPropsType & MapDispatchPropsType
+type PropsType = RouteComponentProps<PathParamsType> & OwnPropsType
+
+class ProfileContainer extends React.Component<PropsType> {
+  componentDidMount() {
+    let userId: number | null = +this.props.match.params.userId;
+    if (!userId) {
+      //     userId = this.props.authorizedUserId!.toString()
+      userId = this.props.authorizedUserId
+      if (!userId) {
+        this.props.history.push("/login");
+      }
+    }
+
+    if (!userId) {
+      console.error("ID should exists in URI params or in state ('authorizedUserId')");
+    } else {
+      this.props.getUserProfile(userId)
+      this.props.getStatus(userId)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus} isAuth={this.props.isAuth}/>
+      </div>
+    )
+  }
+}
+
+let mapStateToProps = (state: AppStateType) => ({
+  profile: state.profilePage.profile,
+  status: state.profilePage.status,
+  authorizedUserId: state.auth.id,
+  isAuth: state.auth.isAuth
+})
+
+export default compose<React.ComponentType>(
+  connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
+  withRouter,
+  // withAuthRedirect // login on the profile page
+)(ProfileContainer)
+
+
+/*
+import React from "react";
+import Profile from "./Profile";
+import {connect} from "react-redux";
+import {
+  getStatus,
+  getUserProfile,
   ProfileType, updateStatus,
 } from "../../redux/profileReducer";
 import {AppStateType} from "../../redux/redux-store";
@@ -60,4 +130,4 @@ export default compose<React.ComponentType>(
   connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
   withRouter,
  // withAuthRedirect // login on the profile page
-)(ProfileContainer)
+)(ProfileContainer)*/
