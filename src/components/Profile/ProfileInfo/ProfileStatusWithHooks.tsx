2fab2bb97bf4843df15eaeca451d851fa9c2fe3d
seomaks <m.voevodin88@gmail.com>
2022-01-27 09:03:32 +0300

    Added ProfileStatus with hooks

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,39 @@
+import React, {ChangeEvent, useState} from "react";
+
+type PropsType = {
+  status: string
+  updateStatus: (status: string) => void
+}
+
+const ProfileStatusWithHooks = (props: PropsType) => {
+  const [editMode, setEditMode] = useState<boolean>(false)
+  const [status, setStatus] = useState<string>(props.status)
+
+  const activateEditMode = () => {
+    setEditMode(true)
+  }
+    const deactivateEditMode = () => {
+      setEditMode(false)
+      props.updateStatus(status)
+    }
+    const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setStatus(e.currentTarget.value)
+    }
+
+    return (
+      <div>
+        {!editMode &&
+          <div>
+            <span
+              onDoubleClick={activateEditMode}>{props.status || "your status is here"}</span>
+          </div>}
+        {editMode &&
+          <div>
+            <input onChange={onStatusChange} autoFocus
+                   onBlur={deactivateEditMode} value={status}/>
+          </div>}
+      </div>
+    )
+}
+
+export default ProfileStatusWithHooks
