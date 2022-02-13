4905c61d7b19693b6ef912d7271d8747885f2479
seomaks <m.voevodin88@gmail.com>
2022-02-13 11:56:49 +0300

    Added Redux DevTools

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -1,10 +1,16 @@
-import {Action, applyMiddleware, combineReducers, createStore} from "redux";
+import {
+  Action,
+  applyMiddleware,
+  combineReducers,
+  compose,
+  createStore
+} from "redux";
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import {usersReducer} from "./usersReducer";
 import {authReducer} from "./authReducer";
 import thunkMiddleware, {ThunkAction} from "redux-thunk";
-import { reducer as formReducer} from 'redux-form'
+import {reducer, reducer as formReducer} from 'redux-form'
 import {appReducer} from "./appReducer";
 
 
@@ -16,9 +22,12 @@ let rootReducer = combineReducers({
   form: formReducer,
   app: appReducer
 })
+//@ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+//export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export type AppStateType = ReturnType<typeof rootReducer>;
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 export type BaseThunkType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
 
 //@ts-ignore
